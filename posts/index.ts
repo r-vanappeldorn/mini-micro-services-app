3a248d292648a513203964e1fe04f7e5b344e165
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-18 20:24:55 +0100

    fix: fix formatting

--- posts/index.ts
+++ posts/index.ts
@@ -1,33 +1,37 @@
 import express from "express";
-import { randomBytes } from "crypto"
-import bodyParser from "body-parser"
+import { randomBytes } from "crypto";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import { CreatePostReq, Posts } from "./types";
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts: Posts = {}
+const posts: Posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+app.get("/posts", (req, res) => {
+  res.send(Object.values(posts));
+});
 
-app.post('/posts', (req: CreatePostReq, res) => {
-    // Create fake id. Would not use this method in a real project.
-    const id = randomBytes(4).toString('hex')
-    const { title } = req.body
+app.post("/posts", (req: CreatePostReq, res) => {
+  // Create fake id. Would not use this method in a real project.
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    const newPost = {
-        id,
-        title
-    }
+  const newPost = {
+    id,
+    title,
+  };
 
-    // Create new entry in posts object.
-    posts[id] = newPost
-    res.status(201).send(newPost)
-})
+  // Create new entry in posts object.
+  posts[id] = newPost;
+  res.status(201).send(newPost);
+
+  console.log("post created");
+});
 
 app.listen(4000, () => {
-    console.log("Listening on port 4000")
-})
+  console.log("Listening on port 4000");
+});
