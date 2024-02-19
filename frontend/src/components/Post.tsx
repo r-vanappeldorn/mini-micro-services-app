55261daf4d1a53fbf3ae5993b9627e3477b5aedb
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-19 18:25:20 +0100

    feat: change icon size and implement commentsList

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,13 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import CommentIcon from "@/icons/CommentIcon";
 import { Post } from "@/types/posts";
-import { CreateCommentForm } from "./CreateCommentForm";
+import axios from "axios";
+import { Comments } from "@/types/comments";
+import CommentsList from "./CommentsList";
 
 export default function Post({ id, title }: Post) {
     const [showComments, setShowComments] = useState(false);
+    const [commentsList, setCommentsList] = useState<Comments>([]);
+
+    useEffect(() => {
+        const getCommentList = async () => {
+            try {
+                const { data: fetchedCommentsList } = await axios.get(
+                    `http://localhost:4001/posts/${id}/comments`,
+                );
+                setCommentsList(fetchedCommentsList);
+            } catch (err) {
+                throw console.error(err);
+            }
+        };
+
+        getCommentList();
+    }, []);
 
     return (
         <div
@@ -15,24 +33,24 @@ export default function Post({ id, title }: Post) {
             className="animate-fade-in mb-2 flex cursor-pointer flex-col border-b border-gray-700"
         >
             <h3 className="text-lg text-gray-200">{title}</h3>
-            <div
-                className={`grid ${showComments ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} transition-grid-rows mb-1`}
-            >
-                <div className="overflow-hidden">
-                    <p className=" text-sm text-gray-400">comment</p>
-                    <CreateCommentForm />
-                </div>
-            </div>
+            <CommentsList
+                postId={id}
+                setCommentsList={setCommentsList}
+                commentsList={commentsList}
+                showComments={showComments}
+            />
             <button
                 className="mb-2 flex"
                 onClick={() => setShowComments(!showComments)}
             >
                 <CommentIcon
-                    height={15}
-                    width={16}
+                    height={17}
+                    width={18}
                     className=" fill-gray-400"
                 />
-                <span className="ml-1 text-[11px] text-gray-400">2</span>
+                <span className="ml-1 text-[12px] text-gray-400">
+                    {commentsList.length}
+                </span>
             </button>
         </div>
     );
