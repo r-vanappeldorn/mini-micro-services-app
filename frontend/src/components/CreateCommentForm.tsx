ace9bc43d667326c3e4005e038c70368e107e34e
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-19 15:22:20 +0100

    fix: prevent submit button from stretching

--- frontend/src/components/CreateCommentForm.tsx
+++ frontend/src/components/CreateCommentForm.tsx
@@ -6,13 +6,19 @@ export function CreateCommentForm() {
     const [comment, setComment] = useState("");
 
     return (
-        <form className="overflow-hidden">
+        <form className="flex overflow-hidden">
             <Input
                 isTextarea
                 id="create-comment"
                 value={comment}
                 setValue={setComment}
             />
+            <button
+                type="submit"
+                className="ml-2 flex flex-grow-0 self-start rounded-sm bg-purple-600 px-2 py-1 text-sm text-gray-200 shadow transition duration-200 ease-in-out hover:bg-purple-700"
+            >
+                Add
+            </button>
         </form>
     );
 }
