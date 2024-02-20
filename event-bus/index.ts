8b6a8daaca97c8b93cb7ecfce1828a2528e2b4c1
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-20 14:24:00 +0100

    feat: add packages and events route in event-bus

---
+++ event-bus/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+    const event = req.body;
+
+    fetch("http://localhost:4000/events", {
+        method: "POST",
+        body: event,
+    });
+    fetch("http://localhost:4001/events", {
+        method: "POST",
+        body: event,
+    });
+    fetch("http://localhost:4002/events", {
+        method: "POST",
+        body: event,
+    });
+
+    res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+    console.log("event bus is running on PORT: 4005")
+})
