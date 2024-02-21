64a40a9653cbb30155b6762f1043d8ecfa0a9512
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-21 16:39:12 +0100

    feat: add event handlers

--- query/index.ts
+++ query/index.ts
@@ -9,19 +9,43 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+
+const handlePostCreated = (newPostEventData: { id: string; title: string }) => {
+    const { id, title } = newPostEventData;
+
+    // Add new post to posts object and add empty array of comments to post.
+    posts[id] = { id, title, comments: [] };
+};
+
+const handleCommentCreated = (newCommentEventData: {
+    id: string;
+    content: string;
+    postId: string;
+}) => {
+    const { postId, id, content } = newCommentEventData;
+
+    // Find post and add new comment to comments array.
+    const post = posts[postId];
+    post.comments.push({ id, content });
+};
 
 app.post("/events", (req: EventReq, res) => {
-    const { type } = req.body;
+    const { type, data } = req.body;
 
     switch (type) {
         case "postCreated":
+            handlePostCreated(data);
             break;
+
         case "commentCreated":
+            handleCommentCreated(data);
             break;
-        default:
-            console.error(`no implementation for event type: ${type}`);
     }
+
+    res.send(200);
 });
 
 app.listen(4002, () => {
