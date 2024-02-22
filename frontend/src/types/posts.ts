70a89d2d107f0f31558fc35bc3d5ab9839f768c9
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-22 16:59:26 +0100

    feat: add comments to post type

--- frontend/src/types/posts.ts
+++ frontend/src/types/posts.ts
@@ -1,6 +1,9 @@
+import { Comments } from "./comments";
+
 export interface Post {
-  id: string;
-  title: string;
+    id: string;
+    title: string;
+    comments: Comments;
 }
 
 export type Posts = Array<Post>;
