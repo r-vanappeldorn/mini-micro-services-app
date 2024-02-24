d00c7d1b84b209af431b3cf6a616165c0fca4f35
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-24 16:35:08 +0100

    feat: add status prop to frontend

--- frontend/src/types/comments.ts
+++ frontend/src/types/comments.ts
@@ -2,6 +2,13 @@ export type Comment = {
     id: string;
     content: string;
     postId: string;
+    status: CommentStatuses
 };
+
+export enum CommentStatuses {
+    "PENDING" = "pending",
+    "APPROVED" = "approved",
+    "REJECTED" = "rejected",
+}
 
 export type Comments = Comment[];
