1853fd2c2ab8f8c5187e8b811fb6718e7105a8da
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-24 16:35:29 +0100

    feat: handle status in comments

--- frontend/src/components/CommentsList.tsx
+++ frontend/src/components/CommentsList.tsx
@@ -1,7 +1,7 @@
 import { Dispatch } from "react";
 
 import { CreateCommentForm } from "./CreateCommentForm";
-import { Comments } from "@/types/comments";
+import { CommentStatuses, Comments } from "@/types/comments";
 
 type CommentsListProps = {
     commentsList: Comments;
@@ -16,15 +16,28 @@ export default function CommentsList({
     showComments,
     setCommentsList,
 }: CommentsListProps) {
+    const renderContent = (status: CommentStatuses, content: string) => {
+        switch (status) {
+            case CommentStatuses.PENDING: {
+                return "Comment review pending";
+            }
+            case CommentStatuses.REJECTED: {
+                return "Comment was rejected";
+            }
+            default:
+                return content;
+        }
+    };
+
     return (
         <div
-            className={`grid ${showComments ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} transition-grid-rows mb-1`}
+            className={`grid ${showComments ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} mb-1 transition-grid-rows`}
         >
             <div className="overflow-hidden">
                 <ul className="list-disc text-gray-400">
-                    {commentsList.map(({ content, id }) => (
+                    {commentsList.map(({ status, content, id }) => (
                         <li className=" text-sm " key={id}>
-                            {content}
+                            {renderContent(status, content)}
                         </li>
                     ))}
                 </ul>
